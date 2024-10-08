4ef4be3c4b1cc1a21e548d78a411c2953dab9496
fgmcolas <https://github.com/fgmcolas>
2024-10-08 13:36:15 +0200

    more documentation ? (/6)

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,56 +1,48 @@
 #!/usr/bin/python3
 """
-script that reads stdin line by line and computes metrics
+Task - Script that reads stdin line by line and computes metrics
 """
+
 import sys
-import signal
-
-# Initialize global variables
-total_file_size = 0
-status_code_counts = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
-line_count = 0
 
 
-def print_statistics():
-    """
-    Prints the current statistics.
-    """
-    print("File size: {}".format(total_file_size))
-    for code in sorted(status_code_counts.keys()):
-        if status_code_counts[code] > 0:
-            print("{}: {}".format(code, status_code_counts[code]))
+if __name__ == "__main__":
+    st_code = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
+    count = 1
+    file_size = 0
 
+    def parse_line(line):
+        """ Read, parse and grab data"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in st_code.keys():
+                st_code[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
 
-def signal_handler(sig, frame):
-    """
-    Prints the stats obtained from the file when a keyboard interrupt occurs.
-    """
-    print_statistics()
+    def print_stats():
+        """print stats in ascending order"""
+        print("File size: {}".format(file_size))
+        for key in sorted(st_code.keys()):
+            if st_code[key]:
+                print("{}: {}".format(key, st_code[key]))
 
-
-# Register the signal handler for SIGINT
-signal.signal(signal.SIGINT, signal_handler)
-try:
-    for line in sys.stdin:
-        line_count += 1
-        line_parsed = line.split()
-        length_line = len(line_parsed)
-        if length_line < 2:
-            continue
-        total_file_size += int(line_parsed[length_line - 1])
-        if line_parsed[length_line - 2] not in status_code_counts.keys():
-            continue
-        status_code_counts[line_parsed[length_line - 2]] += 1
-        if line_count % 10 == 0:
-            print_statistics()
-finally:
-    print_statistics()
+    try:
+        for line in sys.stdin:
+            file_size += parse_line(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
