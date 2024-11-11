f793106cb83c4c98fc7f4658457875cb76cd177f
fgmcolas <https://github.com/fgmcolas>
2024-11-11 16:28:01 +0100

    betty corrected

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -7,28 +7,28 @@
 
 void menger(int level)
 {
-    int n, m, j, x, y;
-    char s;
+	int n, m, j, x, y;
+	char s;
 
-    m = pow(3, level);
-    for (n = 0; n < m; n++)
-    {
-        for (j = 0; j < m;)
-        {
-            s = '#';
-            x = n;
-            y = j++;
-            while (x > 0 || y > 0)
-            {
-                if (x % 3 == 1 && y % 3 == 1)
-                {
-                    s = ' ';
-                }
-                x /= 3;
-                y /= 3;
-            }
-            printf("%c", s);
-        }
-        printf("\n");
-    }
+	m = pow(3, level);
+	for (n = 0; n < m; n++)
+	{
+		for (j = 0; j < m;)
+		{
+			s = '#';
+			x = n;
+			y = j++;
+			while (x > 0 || y > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+				{
+					s = ' ';
+				}
+				x /= 3;
+				y /= 3;
+			}
+			printf("%c", s);
+		}
+		printf("\n");
+	}
 }
