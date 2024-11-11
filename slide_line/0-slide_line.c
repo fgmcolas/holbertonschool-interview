4b5c1dbb2f608c2ff2dab4e43ae4994d370a349f
fgmcolas <https://github.com/fgmcolas>
2024-11-11 16:29:21 +0100

    Switching content files

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,14 +1,107 @@
-#ifndef SLIDE_LINE_H
-#define SLIDE_LINE_H
+#include "slide_line.h"
 
-#define SLIDE_LEFT 2
-#define SLIDE_RIGHT 1
+/**
+ * slide_line - Slides and merges an array to the left or right
+ *
+ * @line: Array on integers
+ * @size: Size of the array
+ * @direction: Direction to slide
+ * Return: 1 if success, 0 otherwise
+ */
 
-#include <stdio.h>
-#include <stdlib.h>
+int slide_line(int *line, size_t size, int direction)
+{
+	if (direction == 2)
+		return (to_left(line, size));
+	if (direction == 1)
+		return (to_right(line, size));
+	else
+		return (0);
+}
 
-int slide_line(int *line, size_t size, int direction);
-int to_left(int *line, size_t size);
-int to_right(int *line, size_t size);
+/**
+ * to_left - Slides and merges array elements to the left
+ *
+ * @line: Array on integers
+ * @size: Size of the array
+ * Return: 1 on success
+ */
 
-#endif
+int to_left(int *line, size_t size)
+{
+	int cur = 0, nxt = 0;
+	size_t i, index = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (line[i] != 0 && cur == 0)
+			cur = line[i];
+		else if (cur != 0 && line[i] != 0)
+			nxt = line[i];
+		if (cur != 0 && nxt != 0)
+		{
+			if (cur == nxt)
+			{
+				line[index++] = cur + nxt;
+				cur = 0;
+				nxt = 0;
+			}
+			else
+			{
+				line[index++] = cur;
+				cur = nxt;
+				nxt = 0;
+				if (i == size - 1)
+					line[index++] = cur;
+			}
+		}
+		if (cur != nxt && i == size - 1)
+			line[index++] = cur;
+	}
+	for (i = index; i < size; i++)
+		line[i] = 0;
+	return (1);
+}
+
+/**
+ * to_right - Slides and merges array elements to the right
+ * @line: Array of integers
+ * @size: Size of the array
+ * Return: 1 on success
+ */
+
+int to_right(int *line, size_t size)
+{
+	int cur = 0, previous = 0;
+	size_t i, index = size - 1;
+
+	for (i = size - 1; i < size; i--)
+	{
+		if (line[i] != 0 && cur == 0)
+			cur = line[i];
+		else if (cur != 0 && line[i] != 0)
+			previous = line[i];
+		if (cur != 0 && previous != 0)
+		{
+			if (cur == previous)
+			{
+				line[index--] = cur + previous;
+				cur = 0;
+				previous = 0;
+			}
+			else
+			{
+				line[index--] = cur;
+				cur = previous;
+				previous = 0;
+				if (i == 0)
+					line[index--] = cur;
+			}
+		}
+		else if (cur != previous && i == 0)
+			line[index--] = cur;
+	}
+	for (i = 0; i < index + 1; i++)
+		line[i] = 0;
+	return (1);
+}
