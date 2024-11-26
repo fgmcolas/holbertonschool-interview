24bd28998f2231da9edc18a4629b8563e35b7ada
fgmcolas <https://github.com/fgmcolas>
2024-11-26 18:51:48 +0100

    Switching spaces with tabs

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -14,31 +14,31 @@
 
 binary_tree_t *add_node(int *array, size_t size, avl_t *parent)
 {
-    binary_tree_t *node;
+	binary_tree_t *node;
 
-    node = malloc(sizeof(avl_t));
-    if (node == NULL)
-        return (NULL);
+	node = malloc(sizeof(avl_t));
+	if (node == NULL)
+		return (NULL);
 
-    node->n = array[(size - 1) / 2];
-    node->right = NULL;
-    node->left = NULL;
-    node->parent = parent;
+	node->n = array[(size - 1) / 2];
+	node->right = NULL;
+	node->left = NULL;
+	node->parent = parent;
 
-    if (size > 1)
-    {
-        node->right = add_node(array + (size + 1) / 2, size / 2, node);
-        if (node->right == NULL)
-            return (NULL);
-    }
-    if (size > 2)
-    {
-        node->left = add_node(array, (size - 1) / 2, node);
-        if (node->right == NULL)
-            return (NULL);
-    }
+	if (size > 1)
+	{
+		node->right = add_node(array + (size + 1) / 2, size / 2, node);
+		if (node->right == NULL)
+			return (NULL);
+	}
+	if (size > 2)
+	{
+		node->left = add_node(array, (size - 1) / 2, node);
+		if (node->right == NULL)
+			return (NULL);
+	}
 
-    return (node);
+	return (node);
 }
 
 /**
@@ -52,11 +52,11 @@ binary_tree_t *add_node(int *array, size_t size, avl_t *parent)
 
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (!array)
-        return NULL;
+	if (!array)
+		return NULL;
 
-    if (!size)
-        return NULL;
+	if (!size)
+		return NULL;
 
-    return ((avl_t *)add_node(array, size, NULL));
+	return ((avl_t *)add_node(array, size, NULL));
 }
