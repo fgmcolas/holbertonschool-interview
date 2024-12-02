e2901fe75a1f8fd961b4eec0850f1886433c944e
fgmcolas <https://github.com/fgmcolas>
2024-12-02 13:04:21 +0100

    Tabs instead of spaces

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -9,38 +9,38 @@
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *express;
+	skiplist_t *express;
 
-    if (list != NULL)
-    {
-        express = list->express;
-        printf("Value checked at index [%lu] = [%d]\n", express->index,
-               express->n);
-        while (express != NULL && express->n < value)
-        {
-            list = express;
-            express = express->express;
-            if (express == NULL)
-                break;
-            printf("Value checked at index [%lu] = [%d]\n", express->index,
-                   express->n);
-        }
-        if (express == NULL)
-        {
-            express = list;
-            while (express->next != NULL)
-                express = express->next;
-        }
-        printf("Value found between indexes [%lu] and [%lu]\n", list->index,
-               express->index);
-        while (list != NULL && list->n <= express->n)
-        {
-            printf("Value checked at index [%lu] = [%d]\n", list->index,
-                   list->n);
-            if (list->n == value)
-                return (list);
-            list = list->next;
-        }
-    }
-    return (NULL);
+	if (list != NULL)
+	{
+		express = list->express;
+		printf("Value checked at index [%lu] = [%d]\n", express->index,
+			   express->n);
+		while (express != NULL && express->n < value)
+		{
+			list = express;
+			express = express->express;
+			if (express == NULL)
+				break;
+			printf("Value checked at index [%lu] = [%d]\n", express->index,
+				   express->n);
+		}
+		if (express == NULL)
+		{
+			express = list;
+			while (express->next != NULL)
+				express = express->next;
+		}
+		printf("Value found between indexes [%lu] and [%lu]\n", list->index,
+			   express->index);
+		while (list != NULL && list->n <= express->n)
+		{
+			printf("Value checked at index [%lu] = [%d]\n", list->index,
+				   list->n);
+			if (list->n == value)
+				return (list);
+			list = list->next;
+		}
+	}
+	return (NULL);
 }
