c061e0e26cca4478f06dfeac79200413d5a92651
fgmcolas <https://github.com/fgmcolas>
2024-12-16 23:32:58 +0100

    editing method

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -17,24 +17,22 @@ def rain(walls):
     if wall_count == 0 or wall_count == 1 or wall_count == 2:
         return 0
 
+    left, right = 0, len(walls) - 1
+    left_max, right_max = walls[left], walls[right]
     total_water = 0
-    while wall_count >= 3:
-        sorted_heights = walls.copy()
-        sorted_heights.sort()
-        highest_wall = sorted_heights[-1]
-        hightest_wall2 = sorted_heights[-2]
-        highest_index = walls.index(highest_wall)
-        hightest_index2 = walls.index(hightest_wall2)
-        if highest_index == hightest_index2:
-            hightest_index2 = walls.index(hightest_index2, highest_index + 1)
-        indices = [highest_index, hightest_index2]
-        indices.sort()
-        lower_wall_height = min(walls[indices[0]], walls[indices[1]])
-        for i in range(indices[0] + 1, indices[1]):
-            total_water += lower_wall_height - walls[i]
-        for i in range(indices[0], indices[1] + 1):
-            del walls[indices[0]]
-        walls.insert(indices[0], lower_wall_height)
-        wall_count = len(walls)
+
+    while left < right:
+        if walls[left] < walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                total_water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                total_water += right_max - walls[right]
+            right -= 1
 
     return total_water
