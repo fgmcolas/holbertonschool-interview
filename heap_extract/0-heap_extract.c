18430f0dad91393658d4ae421e0ca131edfc6012
fgmcolas <https://github.com/fgmcolas>
2025-01-13 18:36:07 +0100

    Betty coding style

--- heap_extract/0-heap_extract.c
+++ heap_extract/0-heap_extract.c
@@ -7,12 +7,12 @@
  */
 static size_t bt_height(const binary_tree_t *tree)
 {
-    size_t h_left;
-    size_t h_right;
+	size_t h_left;
+	size_t h_right;
 
-    h_left = tree->left ? 1 + bt_height(tree->left) : 0;
-    h_right = tree->right ? 1 + bt_height(tree->right) : 0;
-    return (h_left > h_right ? h_left : h_right);
+	h_left = tree->left ? 1 + bt_height(tree->left) : 0;
+	h_right = tree->right ? 1 + bt_height(tree->right) : 0;
+	return (h_left > h_right ? h_left : h_right);
 }
 
 /**
@@ -24,15 +24,15 @@ static size_t bt_height(const binary_tree_t *tree)
  */
 void bt_preorder(heap_t *root, heap_t **node, size_t h, size_t l)
 {
-    if (!root)
-        return;
-    if (h == l)
-        *node = root;
-    l++;
-    if (root->left)
-        bt_preorder(root->left, node, h, l);
-    if (root->right)
-        bt_preorder(root->right, node, h, l);
+	if (!root)
+		return;
+	if (h == l)
+		*node = root;
+	l++;
+	if (root->left)
+		bt_preorder(root->left, node, h, l);
+	if (root->right)
+		bt_preorder(root->right, node, h, l);
 }
 
 /**
@@ -42,26 +42,26 @@ void bt_preorder(heap_t *root, heap_t **node, size_t h, size_t l)
  */
 heap_t *bt_sorting(heap_t *tmp)
 {
-    int aux;
+	int aux;
 
-    while (tmp->left || tmp->right)
-    {
-        if (!tmp->right || tmp->left->n > tmp->right->n)
-        {
-            aux = tmp->n;
-            tmp->n = tmp->left->n;
-            tmp->left->n = aux;
-            tmp = tmp->left;
-        }
-        else if (!tmp->left || tmp->left->n < tmp->right->n)
-        {
-            aux = tmp->n;
-            tmp->n = tmp->right->n;
-            tmp->right->n = aux;
-            tmp = tmp->right;
-        }
-    }
-    return (tmp);
+	while (tmp->left || tmp->right)
+	{
+		if (!tmp->right || tmp->left->n > tmp->right->n)
+		{
+			aux = tmp->n;
+			tmp->n = tmp->left->n;
+			tmp->left->n = aux;
+			tmp = tmp->left;
+		}
+		else if (!tmp->left || tmp->left->n < tmp->right->n)
+		{
+			aux = tmp->n;
+			tmp->n = tmp->right->n;
+			tmp->right->n = aux;
+			tmp = tmp->right;
+		}
+	}
+	return (tmp);
 }
 
 /**
@@ -71,26 +71,26 @@ heap_t *bt_sorting(heap_t *tmp)
  */
 int heap_extract(heap_t **root)
 {
-    int value;
-    heap_t *tmp, *node;
+	int value;
+	heap_t *tmp, *node;
 
-    if (!root || !*root)
-        return (0);
-    tmp = *root;
-    value = tmp->n;
-    if (!tmp->left && !tmp->right)
-    {
-        *root = NULL;
-        free(tmp);
-        return (value);
-    }
-    bt_preorder(tmp, &node, bt_height(tmp), 0);
-    tmp = bt_sorting(tmp);
-    tmp->n = node->n;
-    if (node->parent->right)
-        node->parent->right = NULL;
-    else
-        node->parent->left = NULL;
-    free(node);
-    return (value);
+	if (!root || !*root)
+		return (0);
+	tmp = *root;
+	value = tmp->n;
+	if (!tmp->left && !tmp->right)
+	{
+		*root = NULL;
+		free(tmp);
+		return (value);
+	}
+	bt_preorder(tmp, &node, bt_height(tmp), 0);
+	tmp = bt_sorting(tmp);
+	tmp->n = node->n;
+	if (node->parent->right)
+		node->parent->right = NULL;
+	else
+		node->parent->left = NULL;
+	free(node);
+	return (value);
 }
