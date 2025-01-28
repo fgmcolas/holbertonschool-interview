f0299450a0c692687eb56498e65330fad7edaabc
fgmcolas <https://github.com/fgmcolas>
2025-01-28 14:06:07 +0100

    betty

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -9,24 +9,24 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *p1, *p2;
+	listint_t *p1, *p2;
 
-    if (!head)
-        return (NULL);
+	if (!head)
+		return (NULL);
 
-    p2 = head->next;
-    for (p1 = p2->next; p2 && p1 && p1->next; p1 = p1->next->next)
-    {
-        if (p2 == head || p1 == head)
-            return (head);
+	p2 = head->next;
+	for (p1 = p2->next; p2 && p1 && p1->next; p1 = p1->next->next)
+	{
+		if (p2 == head || p1 == head)
+			return (head);
 
-        if (p2 == p1 && p1 != head)
-        {
-            head = head->next;
-            p2 = head;
-            p1 = head;
-        }
-        p2 = p2->next;
-    }
-    return (NULL);
+		if (p2 == p1 && p1 != head)
+		{
+			head = head->next;
+			p2 = head;
+			p1 = head;
+		}
+		p2 = p2->next;
+	}
+	return (NULL);
 }
