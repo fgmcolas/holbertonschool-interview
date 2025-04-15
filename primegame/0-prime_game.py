dc90f7fd83b922a8d090f65f3d05b15889738b8c
fgmcolas <https://github.com/fgmcolas>
2025-04-15 14:06:27 +0200

    Task 0

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -3,38 +3,45 @@
 
 
 def isWinner(x, nums):
-    if not nums or x < 1:
+    """
+    Determines the winner of each round and returns the player
+    with the most wins.
+    """
+    if not nums or x < 1 or len(nums) != x:
         return None
 
-    max_num = max(nums)
+    max_n = max(nums)
+    # Generate prime numbers using Sieve of Eratosthenes
+    sieve = [True] * (max_n + 1)
+    sieve[0] = sieve[1] = False
 
-    # Sieve of Eratosthenes
-    primes = [True for _ in range(max_num + 1)]
-    primes[0] = primes[1] = False
+    for i in range(2, int(max_n ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, max_n + 1, i):
+                sieve[j] = False
 
-    for i in range(2, int(max_num ** 0.5) + 1):
-        if primes[i]:
-            for j in range(i * i, max_num + 1, i):
-                primes[j] = False
-
-    # Precompute number of primes up to each i
-    prime_count = [0] * (max_num + 1)
+    # Precompute number of primes up to each n
+    primes_up_to = [0] * (max_n + 1)
     count = 0
-    for i in range(1, max_num + 1):
-        if primes[i]:
+    for i in range(1, max_n + 1):
+        if sieve[i]:
             count += 1
-        prime_count[i] = count
+        primes_up_to[i] = count
 
-    # Play the game
-    maria = ben = 0
+    maria_wins = 0
+    ben_wins = 0
+
     for n in nums:
-        if prime_count[n] % 2 == 0:
-            ben += 1
+        if n < 1:
+            continue  # Skip invalid rounds
+        if primes_up_to[n] % 2 == 1:
+            maria_wins += 1
         else:
-            maria += 1
+            ben_wins += 1
 
-    if maria > ben:
+    if maria_wins > ben_wins:
         return "Maria"
-    elif ben > maria:
+    elif ben_wins > maria_wins:
         return "Ben"
-    return None
+    else:
+        return None
