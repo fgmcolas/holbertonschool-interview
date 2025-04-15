8221cf965f42a45f7cfce7a6fe7cf6c4b85db516
fgmcolas <https://github.com/fgmcolas>
2025-04-15 14:04:16 +0200

    Task 0

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -2,62 +2,39 @@
 """ Prime Game """
 
 
-def isprime(n):
-    """ n: number to check if it is prime"""
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
-
-def delete_numbers(n, nums):
-    """ delete numbers - assign zero """
-    for i in range(len(nums)):
-        if nums[i] % n == 0:
-            nums[i] = 0
-
-
 def isWinner(x, nums):
-    """ where x is the number of rounds and nums is an array of n
-        Return: name of the player that won the most rounds
-        Iriaf the winner cannot be determined, return None
-        You can assume n and x will not be larger than 10000
-    """
-    nums.sort()
-    winner = False
-    Maria = 0
-    Ben = 0
-    for game in range(x):
-        # print("game# ", game+1)
-        nums2 = list(range(1, nums[game] + 1))
-        # print("nums: ", nums2)
-        turn = 0
-        while True:
-            """
-            # uncomment to monitor turns
-            if turn % 2 != 0:
-                print("Ben turn ")
-            else:
-                print("Maria turn ")
-            """
-            change = False
-            for i, n in enumerate(nums2):
-                # print("n: ", n, "i: ", i)
-                if n > 1 and isprime(n):
-                    delete_numbers(n, nums2)
-                    change = True
-                    turn += 1
-                    break
-            # print("movement: ", nums2)
-            if change is False:
-                break
-        if turn % 2 != 0:
-            Maria += 1
-        else:
-            Ben += 1
-        # print("Maria: {}, Ben: {}".format(Maria, Ben))
-    if Maria == Ben:
+    if not nums or x < 1:
         return None
-    if Maria > Ben:
+
+    max_num = max(nums)
+
+    # Sieve of Eratosthenes
+    primes = [True for _ in range(max_num + 1)]
+    primes[0] = primes[1] = False
+
+    for i in range(2, int(max_num ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, max_num + 1, i):
+                primes[j] = False
+
+    # Precompute number of primes up to each i
+    prime_count = [0] * (max_num + 1)
+    count = 0
+    for i in range(1, max_num + 1):
+        if primes[i]:
+            count += 1
+        prime_count[i] = count
+
+    # Play the game
+    maria = ben = 0
+    for n in nums:
+        if prime_count[n] % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+
+    if maria > ben:
         return "Maria"
-    return "Ben"
+    elif ben > maria:
+        return "Ben"
+    return None
